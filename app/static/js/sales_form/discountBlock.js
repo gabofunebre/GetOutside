a9d7c7b0d18e830c54f855ecf509399a8d61c3bd
gabofunebre <136429132+gabofunebre@users.noreply.github.com>
2025-06-10 01:53:24 +0100

    Remove amount labels from blocks

--- app/static/js/sales_form/discountBlock.js
+++ app/static/js/sales_form/discountBlock.js
@@ -22,20 +22,18 @@ export class DiscountBlock {
   /** Genera el HTML del bloque */
   render() {
     const div = document.createElement('div');
-    div.className = 'descuento-block border rounded p-3 mb-3';
+    div.className = 'descuento-block border rounded p-3 mb-3 position-relative';
     div.innerHTML = `
-      <div class="mb-2">
-        <label class="form-label">Concepto de Descuento</label>
-        <input type="text" name="concepto" class="form-control">
+      <button type="button" class="btn-close btn-sm btn-quitar-descuento position-absolute top-0 end-0 m-2" aria-label="Quitar"></button>
+      <div class="row mb-1">
+        <div class="col"><label class="form-label">Concepto</label></div>
       </div>
-      <div class="row mb-2">
+      <div class="row align-items-end g-2">
         <div class="col">
-          <label class="form-label">Monto</label>
-          <input type="number" name="amount" class="form-control" step="0.01">
+          <input type="text" name="concepto" class="form-control">
         </div>
-        <div class="col-auto d-flex flex-column justify-content-end">
-          <label class="form-label invisible">Quitar</label>
-          <button class="btn btn-danger btn-quitar-descuento">Quitar</button>
+        <div class="col-auto">
+          <input type="number" name="amount" class="form-control amount-input" step="0.01">
         </div>
       </div>
     `;
